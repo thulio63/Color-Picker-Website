7b065b8c8237b19f87bbfc52fece3aa83d41a3c3
Andrew Thul <thulio63@gmail.com>
2025-03-25 15:44:40 -0500

    began interactive query, expanded reference script

--- sqlSubmit.py
+++ sqlSubmit.py
@@ -1,10 +1,13 @@
-import virtualenvs
+#!/Users/andrewthul/Desktop/Projects/Code/Color Picker Website/virtualenvs/bin/python
+
 import mysql.connector
 from mysql.connector import errorcode
 import touchy
+#import sys
+#print(sys.executable)
 
 try:
-    cnx = mysql.connector.connect(user = 'root', password = f'{touchy.password}', host = 'localhost', port = 3306, database = 'colorPickerApp')
+    cnx = mysql.connector.connect(user = touchy.user, password = f'{touchy.password}', host = touchy.host, port = touchy.port, database = touchy.db)
 except mysql.connector.Error as err:
   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
     print("Something is wrong with your user name or password")
@@ -13,12 +16,35 @@ except mysql.connector.Error as err:
   else:
     print(err)
     
-print('successfully accessed database!')
 cursor = cnx.cursor()
+print('successfully accessed database!')
+'''
+#DO NOT GIVE OTHERS ACCESS TO THIS DUMMY
+options = ["SELECT", "INSERT", "CREATE"]
+opts = []
+choosing = True
+while choosing:
+    print("select from the following:\n")
+    for opt in options:
+        print(opt)
+        opts.append(opt)
+    choice = str(input()).upper()
+    for x in range(len(options)):        
+        if choice in opts:
+            print(f"executing {choice}")
+            choosing = False
+            break
+    if choosing:
+        print("\ninvalid choice, please try again\n")
+'''
+#build-a-query for fun, not necessary for this project
 query = 'SELECT * FROM colors'
-#cnx._execute_query(query)
 cursor.execute(query)
+print(f"executing query: {query}")
 rows = cursor.fetchall()
-print(rows)
+print("printing results")
+for row in rows:
+    print(row)
 cursor.close()
+print("goodbye!")
 cnx.close
